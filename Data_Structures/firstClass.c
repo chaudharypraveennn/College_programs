07313ef37b4ce6dc13d7a2206aa74c50c390a7bf
Praveen Chaudhary <chpraveennn@gmail.com>
2025-09-05 19:44:24 +0530

    Deleted .git folder

---
+++ Data_Structures/firstClass.c
@@ -0,0 +1,70 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    void create();
+    void insert();
+    void traverse();
+    create();
+    traverse();
+    insert();
+    traverse();
+    return 0;
+}
+    
+struct node {
+        int info;
+        struct node *link;
+};
+struct node *first;
+    
+    
+void create() {
+    struct node *ptr, *cpt;
+    int num;
+    
+    ptr = (struct node*)malloc(sizeof(struct node));
+    
+    printf("Enter info of ptr: ");
+    scanf("%d", &ptr -> info);
+    
+    first = ptr;
+        
+do {
+    cpt = (struct node*)malloc(sizeof(struct node));
+    
+    printf("enter info of cpt: ");
+    scanf("%d", &cpt -> info);
+    
+    ptr -> link = cpt;
+    ptr = cpt;
+    
+    printf("Do you want more node press '0/1': ");
+    scanf("%d", &num);
+    } while(num == 1);
+        ptr -> link = NULL;
+}
+
+void traverse() {
+    struct node *z;
+    z = first;
+    
+    while(z != NULL) {
+        printf("%d\t%d\n", z->info, z->link);
+        z = z->link;
+    }
+}
+        
+void insert() {
+    struct node *node;
+
+    node = (struct node*)malloc(sizeof(struct node));
+    printf("\nEnter value to insert: ");
+    scanf("%d", &node->info);
+    // node->link = NULL;
+    node->link = first;
+    first =node;
+
+}
+

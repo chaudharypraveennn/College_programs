8bcb2840998937a813bf36dfac3a74bba7c846ac
Praveen Chaudhary <chpraveennn@gmail.com>
2025-10-31 12:03:28 +0530

    Update Deletion in Queue Implement using linkedlist

--- Data_Structures/queue_implement_using_linkedlist.c
+++ Data_Structures/queue_implement_using_linkedlist.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+struct node
+{
     int data;
     struct node *link;
 };
 
 struct node *f, *r;
 
-main() {
+main()
+{
     createQueue();
     display();
 
     insert();
     display();
 
+    delete();
+    display();
+
     return 0;
 }
 
-void createQueue() {
-    int ch; 
+void createQueue()
+{
+    int ch;
     struct node *y, *z;
     y = (struct node *)malloc(sizeof(struct node));
     printf("Enter data of y: ");
@@ -27,7 +33,8 @@ void createQueue() {
 
     f = r = y;
 
-    do {
+    do
+    {
         z = (struct node *)malloc(sizeof(struct node));
         printf("Enter data of z: ");
         scanf("%d", &z->data);
@@ -37,14 +44,16 @@ void createQueue() {
 
         printf("Do you want more node press '0/1': ");
         scanf("%d", &ch);
-    } while(ch == 1);
+    } while (ch == 1);
     r->link = NULL;
 }
 
-void display() {
+void display()
+{
     struct node *temp = f;
     printf("\nQueue elements are: ");
-    while (temp != NULL) {
+    while (temp != NULL)
+    {
         printf("%d ", temp->data);
         temp = temp->link;
     }
@@ -54,17 +63,37 @@ void display() {
     printf("\n");
 }
 
-void insert() {
+void insert()
+{
     struct node *temp;
     temp = (struct node *)malloc(sizeof(struct node));
     printf("Enter data to insert: ");
     scanf("%d", &temp->data);
     temp->link = NULL;
 
-    if (r == NULL) {
+    if (r == NULL)
+    {
         f = r = temp;
-    } else {
+    }
+    else
+    {
         r->link = temp;
         r = temp;
+    }
+}
+
+void delete()
+{
+    struct node *temp;
+
+    if (f == NULL)
+    {
+        printf("Queue is empty.\n");
+    }
+    else
+    {
+        temp = f;
+        f = f->link;
+        free(temp);
     }
 }
